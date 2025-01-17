36a020d8014d848f1a662a623eab57f6f2d3c0fb
Jack <6464072@qq.com>
2025-01-17 14:28:50 +0800

    test:test playground import

---
+++ examples/dob0/0.basic-loot.ts
@@ -0,0 +1,18 @@
+import { ccc } from "@ckb-ccc/ccc";
+import {  signer } from "@ckb-ccc/playground";
+
+
+// Build transaction
+let { tx, id } = await ccc.spore.createSpore({
+    signer,
+    data: {
+        contentType: "text/plain",
+        content: ccc.bytesFrom("hello, spore", "utf8"),
+    },
+});
+console.log("sporeId:", id);
+// Complete transaction
+await tx.completeFeeBy(signer);
+// Send transaction
+const txHash = await signer.sendTransaction(tx);
+console.log(txHash);
