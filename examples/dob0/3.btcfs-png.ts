6a289b54185ba5bac6aee09aef887443f035e927
aabbcc <iyranly@163.com>
2025-01-22 00:24:24 +0800

    feat: add btcfs-png dob0 example

--- examples/dob0/3.btcfs-png.ts
+++ examples/dob0/3.btcfs-png.ts
@@ -1,11 +1,27 @@
 import { ccc } from "@ckb-ccc/ccc";
-import { client, signer, render } from "@ckb-ccc/playground";
+import { client, signer } from "@ckb-ccc/playground";
 
+function getExplorerTxUrl(txHash: string) {
+  const isMainnet = client.addressPrefix === 'ckb';
+  const baseUrl = isMainnet ? 'https://explorer.nervos.org' : 'https://testnet.explorer.nervos.org';
+
+  return `${baseUrl}/transaction/${txHash}`
+}
+
+function generateSimpleDNA(length: number): string {
+  return Array.from(
+    { length }, 
+    () => Math.floor(Math.random() * 16).toString(16)
+  ).join('');
+}
+
+/**
+ * Generate cluster description
+ */
 function generateClusterDescriptionUnderDobProtocol() {
-  /**
-   * Generation example for DOB0
-   */
-  const clusterDescription = "this is the description for Common-image cluster";
+ 
+  const clusterDescription = "A cluster with btcfs png as the primary rendering objects.";
+  
   const dob0Pattern: ccc.spore.dob.PatternElementDob0[] = [
     {
       traitName: "prev.type",
@@ -18,22 +34,40 @@ function generateClusterDescriptionUnderDobProtocol() {
     {
       traitName: "prev.bg",
       dobType: "String",
-      dnaOffset: 6,
+      dnaOffset: 1,
       dnaLength: 1,
       patternType: "options",
-    
       traitArgs:[
-       "btcfs://8330ca2167ee77dcd0bc5589aacd743aba259fd92f63ebc7e4d950ac9e2578a7i0",
+       "btcfs://545b94cb1ecf2175b81c601346e4a7e05149cafc6f235330c9918e35f920e109i0",
       ],
+    },
+    {
+      traitName: "prev.bgcolor",
+      dobType: "String",
+      dnaOffset: 2,
+      dnaLength: 1,
+      patternType: "options",
+      traitArgs:[
+       "#E0E1E2",
+      ],
+    },
+    {
+      traitName: "Type",
+      dobType: "Number",
+      dnaOffset: 3,
+      dnaLength: 1,
+      patternType: "range",
+      traitArgs: [10, 50],
     },
     {
       traitName: "Timestamp",
       dobType: "Number",
-      dnaOffset: 2,
+      dnaOffset: 4,
       dnaLength: 4,
       patternType: "rawNumber",
     },
   ];
+
   const dob0: ccc.spore.dob.Dob0 = {
     description: clusterDescription,
     dob: {
@@ -42,35 +76,120 @@ function generateClusterDescriptionUnderDobProtocol() {
       pattern: dob0Pattern,
     },
   };
+
   return ccc.spore.dob.encodeClusterDescriptionForDob0(dob0);
 }
-const { tx, id: clusterId } = await ccc.spore.createSporeCluster({
+
+/**
+ * create cluster
+ */
+const { tx: clusterTx, id: clusterId } = await ccc.spore.createSporeCluster({
   signer,
   data: {
-    name: "Common Image",
+    name: "BTCFS PNG",
     description: generateClusterDescriptionUnderDobProtocol(),
   },
 });
-await tx.completeFeeBy(signer);
+await clusterTx.completeFeeBy(signer, 2000n);
+const clusterTxHash = await signer.sendTransaction(clusterTx);
+console.log("Create cluster tx sent:", clusterTxHash, `Cluster ID: ${clusterId}`);
+await signer.client.waitTransaction(clusterTxHash);
+console.log("Create cluster tx committed:", getExplorerTxUrl(clusterTxHash), `Cluster ID: ${clusterId}`);
 
-const txHash = await signer.sendTransaction(tx);
-console.log("Transaction sent:", txHash, "Cluster ID:", clusterId);
 /**
-##createCluster
+ * create spore
  */
+//const clusterId = '';
 const { tx: sporeTx, id: sporeId } = await ccc.spore.createSpore({
   signer,
   data: {
     contentType: "dob/0",
-    content: ccc.bytesFrom('{ "dna": "0123456789abcdef" }', "utf8"),
+    content: ccc.bytesFrom(`{ "dna": "${generateSimpleDNA(16)}" }`, "utf8"),
     clusterId: clusterId,
   },
   clusterMode: "clusterCell",
 });
-await sporeTx.completeFeeBy(signer);
-
+await sporeTx.completeFeeBy(signer, 2000n);
 const sporeTxHash = await signer.sendTransaction(sporeTx);
-console.log("Transaction sent:", txHash, "Spore ID:", sporeId);
-await signer.client.waitTransaction(txHash);
-
+console.log("Mint DOB tx sent:", sporeTxHash, `Spore ID: ${sporeId}`);
 await signer.client.waitTransaction(sporeTxHash);
+console.log("Mint DOB tx committed:", getExplorerTxUrl(sporeTxHash), `Spore ID: ${sporeId}`);
+
+
+/**
+ * The code below helps you to view the dob you just minted
+ */
+
+const getDobTypeHash = (sporeId: string, version?: ccc.spore.SporeVersion | ccc.spore.SporeVersion.V2 )  => {
+    const sporeScriptInfo = ccc.spore.getSporeScriptInfo(client, version)
+    const dobTypeScript = ccc.Script.from({
+        codeHash: sporeScriptInfo.codeHash,
+        hashType: sporeScriptInfo.hashType,
+        args: sporeId
+    })
+
+    return dobTypeScript.hash();
+}
+
+const getClusterTypeHash = (clusterId: string, version?: ccc.spore.SporeVersion | ccc.spore.SporeVersion.V2 )  => {
+    const clusterScriptInfo = ccc.spore.getClusterScriptInfo(client, version)
+    const clusterTypeScript = ccc.Script.from({
+        codeHash: clusterScriptInfo.codeHash,
+        hashType: clusterScriptInfo.hashType,
+        args: clusterId
+    })
+
+    return clusterTypeScript.hash();
+}
+
+enum PlatformSupportedDOB {
+  JOYID = "joyid",
+  CKBEXPLORER = "ckb explorer",
+  OMIGA = "omiga",
+  DOBBY = "dobby",
+  MOBIT = "mobit",
+}
+
+const viewDobUrl = (platform : PlatformSupportedDOB, clusterId: string, sporeId: string) => {
+    const isMainnet = client.addressPrefix === 'ckb';
+    let url = ''
+
+    switch (platform) {
+        case PlatformSupportedDOB.JOYID:
+            url = isMainnet 
+                ? `https://app.joy.id/nft/${sporeId.slice(2)}`
+                : `https://testnet.joyid.dev/nft/${sporeId.slice(2)}`;
+            break;
+        case PlatformSupportedDOB.OMIGA:
+            const sporeTypeHash = getDobTypeHash(sporeId);
+            url = isMainnet
+                ? `https://omiga.io/info/dobs/${sporeTypeHash}`
+                : `https://test.omiga.io/info/dobs/${sporeTypeHash}`;
+            break;
+        case PlatformSupportedDOB.CKBEXPLORER:
+            const clusterTypeHash = getClusterTypeHash(clusterId);
+            url = isMainnet
+                ? `https://explorer.nervos.org/nft-info/${clusterTypeHash}/${sporeId}`
+                : `https://testnet.explorer.nervos.org/nft-info/${clusterTypeHash}/${sporeId}`;
+            break;
+        case PlatformSupportedDOB.DOBBY:
+            url = isMainnet
+                ? `https://app.dobby.market/item-detail_ckb/${sporeId}`
+                : `https://test-dobby.entrust3.com/item-detail_ckb/${sporeId}`;
+            break;
+        case PlatformSupportedDOB.MOBIT:
+            url = isMainnet
+                ? `https://mobit.app/dob/${sporeId.slice(2)}?chain=ckb`
+                : `https://mobit.app/dob/${sporeId.slice(2)}?chain=ckb`;
+            break;
+        default:
+            throw new Error(`Unsupported platform: ${platform}`);
+    }
+    
+    return url;
+}
+
+console.log('Now you can view the dob on JoyId, Omiga, CKB Explorer, Mobit, Dobby...');
+Object.values(PlatformSupportedDOB).forEach(platform => {
+    console.log(`View on ${platform}: 👉🔗`, viewDobUrl(platform, clusterId, sporeId));
+});
